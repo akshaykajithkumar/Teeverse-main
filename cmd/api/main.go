085309e2e554574c6f3379e714c2788fe3fa0c83
akshay <akshaykajithkumar@gmail.com>
2023-09-12 02:26:05 +0530

    .

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,11 +12,12 @@ func main() {
 	docs.SwaggerInfo.Description = "universal tshirts "
 	docs.SwaggerInfo.Version = "1.0"
 	docs.SwaggerInfo.Host = "teeverse.online"
-	docs.SwaggerInfo.Host = "localhost:1243"
+	//docs.SwaggerInfo.Host = "localhost:1243"
 	docs.SwaggerInfo.BasePath = ""
 	docs.SwaggerInfo.Schemes = []string{"http", "https"}
 	config, configErr := config.LoadConfig()
 	if configErr != nil {
+
 		log.Fatal("cannot load config: ", configErr)
 	}
 
